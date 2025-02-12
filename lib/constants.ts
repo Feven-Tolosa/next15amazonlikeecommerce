8df6c69d8eb69a2d98c32e051992126121a71e8f
Feven-Tolosa <tolosafeven0@gmail.com>
2025-02-12 21:42:06 +0300

    2. website lay out

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const App_Name = process.env.NEXT_PUBLIC_APP_NAME || 'NexAmzn'
+export const App_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Send less, enjoy more.'
+export const App_Description =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'An Amazon clone build with Next.js and MongoDB.'
---
+++ lib/data.ts
@@ -0,0 +1,37 @@
+const data = {
+  headerMeanus: [
+    {
+      name: "Today's Deal",
+      href: '/search?tag=todays-deal',
+    },
+    {
+      name: 'New Ariivals',
+      href: '/search?tag=new-arrivals',
+    },
+    {
+      name: 'Featured Products',
+      href: '/search?tag=featured-products',
+    },
+    {
+      name: 'Best Sellers',
+      href: '/search?tag=best-sellers',
+    },
+    {
+      name: 'Browsing History',
+      href: '/#browsing-history',
+    },
+    {
+      name: 'customer service',
+      href: '/page/customer-service',
+    },
+    {
+      name: 'About Us',
+      href: '/page/about-us',
+    },
+    {
+      name: 'help',
+      href: '/page/help',
+    },
+  ],
+}
+export default data
